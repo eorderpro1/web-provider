c71b1fcc5bab96d18de49ac2ce8a64374c1b6b1f
Lefteris Vandoros <lvandoros@cognity.gr>
2025-01-22 11:57:50 +0200

    adding orders-overview

---
+++ src/app/views/pages/orders-overview/orders-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders-overview.component.html',
+  styleUrl: './orders-overview.component.scss'
+})
+export class OrdersOverviewComponent {
+
+}
