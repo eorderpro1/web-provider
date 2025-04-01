5e3e9f0b6bd74c513d0cec78b6fa4d8f2060ec20
Lefteris Vandoros <v-evandoros@eurobank.gr>
2025-04-01 22:00:28 +0300

    deleting chat and add some changes for campaigns

--- src/app/views/pages/campaigns/campaigns.component.ts
+++ src/app/views/pages/campaigns/campaigns.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-campaigns',
   standalone: true,
-  imports: [],
+  imports: [NgbAccordionModule],
   templateUrl: './campaigns.component.html',
   styleUrl: './campaigns.component.scss'
 })
